37cd42d901824fe8de9a003f286b3f569a77586a
pzx <panzx9865@qq.com>
2026-09-18 17:37:08 +0800

    add curl tool and lib

--- qemu-virt/scripts/mkver.sh
+++ qemu-virt/scripts/mkver.sh
@@ -12,14 +12,14 @@ echo "rootfs block count: $ROOTFS_BLKCOUNT"
 export ROOTFS_BLKSIZE ROOTFS_BLKCOUNT
 
 cd $ROOT_DIR
-make rootfsimg
+make squashroot
 cd -
 
 #dd if=/dev/zero of=$TOP_DIR/version.bin bs=1M count=128
 
 $CODE_DIR/host/buildversion/buildversion \
     -k $TOP_DIR/kernel.itb \
-    -r $TOP_DIR/rootfs.img \
+    -r $TOP_DIR/rootfs.squash \
     -u $TOP_DIR/upgrade.bin \
     -p $CODE_DIR/rsakeys/pzx.key
     # -v $TOP_DIR/version.bin
